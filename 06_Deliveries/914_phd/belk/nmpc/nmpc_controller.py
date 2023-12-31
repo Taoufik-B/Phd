f745dc8611581890172fdcf503e7b28967f82f5e
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2023-12-31 23:32:24 +0100

    vault backup: 2023-12-31 23:32:24
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/nmpc_controller.py

--- 06_Deliveries/914_phd/belk/nmpc/nmpc_controller.py
+++ 06_Deliveries/914_phd/belk/nmpc/nmpc_controller.py
@@ -2,4 +2,20 @@ import casadi as ca
 
 class NMPCController:
     def __init__(self) -> None:
+        
+        self._setup()
+        pass
+
+    def _setup(self):
+        """
+        Setup the NMPC optimization problem
+        """
+
+        pass
+
+    def compute_controls(self):
+
+        pass
+
+    def run_step(self):
         pass

8df43c871ea1857c294edaa91e482e9d4f248264
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 17:12:15 +0100

    vault backup: 2024-01-01 17:12:15
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/config.py
    06_Deliveries/914_phd/belk/nmpc/configs/test.cfg

--- 06_Deliveries/914_phd/belk/nmpc/config.py
+++ 06_Deliveries/914_phd/belk/nmpc/config.py
@@ -1,2 +1,12 @@
 import configparser
 import os
+
+def load_configuration(path):
+    configParser = configparser.ConfigParser()
+    configParser.read(path)
+    return configParser
+
+
+if __name__ == 'main':
+    config = load_configuration('./configs/test.cfg')
+    print(config)

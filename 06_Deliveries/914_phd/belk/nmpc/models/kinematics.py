d9a01c125e0e163961253ff01e3a422b43be2144
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 18:02:15 +0100

    vault backup: 2024-01-01 18:02:15
    Affected files:
    04_Notes/Phd-KnowledgeBase/.obsidian/workspace.json
    06_Deliveries/914_phd/belk/nmpc/models/kinematics.py

--- 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
+++ 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
@@ -10,21 +10,17 @@ class VehicleKinematicModel:
         self.y = ca.SX.sym('y')
         self.psi = ca.SX.sym('psi')      # the yaw angle / heading angle
         self.delta = ca.SX.sym('delta')  # the steering angle
-        
-        self.beta = ca.SX.sym('beta')    # the slip angle angle / case of desired control point is in the cog
+
         #controls
         self.v = ca.SX.sym('v')
         self.phi = ca.SX.sym('phi') # the steering rate
 
         # State and Control vector
-        self.states = None
+        self.states = ca.vertcat(self.x, self.y, self.psi, self.delta)
         self.controls = ca.vertcat(self.v, self.delta)
         pass
     ### desired point is at the center of the rear axle,
     def kvmodel_rac(self):
-        # State vector 
-        states = ca.vertcat(self.x, self.y, self.psi, self.delta)
-
         # State update equations (kinematic bicycle model)
         rhs = ca.vertcat( self.v * ca.cos(self.psi)
                         , self.v*  ca.sin(self.psi)
@@ -36,9 +32,6 @@ class VehicleKinematicModel:
 
     ### desired point is at the center of the front axle:
     def kvmodel_fac(self):
-        # State vector 
-        states = ca.vertcat(self.x, self.y, self.psi, self.delta)
-
         # State update equations (kinematic bicycle model)
         rhs = ca.vertcat( self.v * ca.cos(self.psi+self.delta)
                         , self.v*  ca.sin(self.psi+self.delta)
@@ -50,10 +43,8 @@ class VehicleKinematicModel:
 
     ### desired point is at the center of gravity:
     def kvmodel_cg(self):
-        # State vector 
-        states = ca.vertcat(self.x, self.y, self.psi, self.delta)
-
         # State update equations (kinematic bicycle model)
+        beta = ca.arctan2
         rhs = ca.vertcat( self.v * ca.cos(self.psi+self.beta)
                         , self.v*  ca.sin(self.psi+self.beta)
                         , self.v/self.L * ca.sin(self.delta)

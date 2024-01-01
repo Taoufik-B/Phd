36535cd2b100466b1080a2f38783b9c58aaa4ea4
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 18:17:15 +0100

    vault backup: 2024-01-01 18:17:15
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/utils/discretize.py

--- 06_Deliveries/914_phd/belk/nmpc/utils/discretize.py
+++ 06_Deliveries/914_phd/belk/nmpc/utils/discretize.py
@@ -1,13 +1,10 @@
-def discretize_rk4(f_function, state_vector):
+def discretize_rk4(f_function, state_vector, control_vector, dt, only_euler=False):
     # st_runge_kutta
-    k1 = self.f(X[:,k], U[:,k])
-    if self.activate_rk4:
-        k2 = self.f(X[:,k]+self.dt/2*k1, U[:,k])
-        k3 = self.f(X[:,k]+self.dt/2*k2, U[:,k])
-        k4 = self.f(X[:,k]+self.dt/2*k3, U[:,k])
-        st_rk4 = X[:,k]+self.dt/6*(k1+2*k2+2*k3+k4)
-        st_next_aprx = st_rk4
-    else:
-        st_next_euler = X[:,k] + k1*self.dt
-        st_next_aprx = st_next_euler
-    st_next = X[:,k+1] 
+    k1 = f_function(state_vector, control_vector)
+    if only_euler:
+        return state_vector + k1*dt
+    k2 = f_function(state_vector+dt/2*k1, control_vector)
+    k3 = f_function(state_vector+dt/2*k2, control_vector)
+    k4 = f_function(state_vector+dt/2*k3, control_vector)
+    
+    return state_vector+dt/6*(k1+2*k2+2*k3+k4)

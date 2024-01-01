b5b4cc3405cce192b059485a4fc2faabaeb8410a
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 18:52:15 +0100

    vault backup: 2024-01-01 18:52:15
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/configs/basic.yaml
    06_Deliveries/914_phd/belk/nmpc/utils/config.py

--- 06_Deliveries/914_phd/belk/nmpc/utils/config.py
+++ 06_Deliveries/914_phd/belk/nmpc/utils/config.py
@@ -8,12 +8,12 @@ def load_configuration(path):
     configParser.read(path)
     return configParser
 def load_yaml_config(path):
-    configParser = configparser.ConfigParser(converters={'list': lambda x: [i.strip() for i in x.split(',')]})
-    configParser.read(path)
-    return configParser
+    with open(path) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
 
 if __name__ == '__main__':
     logging.info("Loading the configuration file")
     config = load_configuration('./configs/basic.cfg')
+    yaml_config = load_yaml_config('./configs/basic.yaml')
     logging.info("Printing configuration file")
     print(dict(config))

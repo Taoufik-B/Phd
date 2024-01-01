02ba48367a83572ae217adc96806139127e317e4
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-02 00:48:42 +0100

    vault backup: 2024-01-02 00:48:42
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/controllers/nmpc_controller.py

--- 06_Deliveries/914_phd/belk/nmpc/controllers/nmpc_controller.py
+++ 06_Deliveries/914_phd/belk/nmpc/controllers/nmpc_controller.py
@@ -24,11 +24,24 @@ class NMPCController:
         P = ca.SX.sym('P', self.model.n_states+self.model.n_opt_vars*self.N) 
 
         #objective
-
+        obj = 0
 
         #constraints
         g = []
         g = ca.vertcat(g,X[:,0]-P[0:self.model.n_states]) #initial condition constraint
+
+        # compute g and obj
+        for k in range(self.N):
+            st_next = X[:,k+1] 
+            # Compute constraints
+            g = ca.vertcat(g, st_next - st_next_aprx)
+            state_error = X[:,k] - P[self.model.n_states+self.model.n_opt_vars*k
+                                     :self.model.n_states+self.model.n_opt_vars*k+self.model.n_states]
+            con = U[:,k] - \
+                P[self.model.n_states+self.model.n_opt_vars*k+self.model.n_states
+                  :self.model.n_states+self.model.n_opt_vars*k+self.model.n_states+self.model.n_controls]
+            obj += state_error.T@self.Q@state_error# State cost
+            obj += con.T@self.R@con # Control cost
 
 
         pass

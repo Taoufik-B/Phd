0f37b6f6d56aefc8d42665981e18561981943ce8
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 17:47:15 +0100

    vault backup: 2024-01-01 17:47:15
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/models/kinematics.py

--- 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
+++ 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
@@ -1,61 +1,68 @@
 import casadi as ca
 
 ## Model definition
+class VehicleKinematicModel:
+    def __init__(self, L) -> None:
+        self.L = L
+        #states
+        self.x = ca.SX.sym('x')
+        self.y = ca.SX.sym('y')
+        self.psi = ca.SX.sym('psi')      # the yaw angle / heading angle
+        self.delta = ca.SX.sym('delta')  # the steering angle
+        
+        self.beta = ca.SX.sym('beta')    # the slip angle angle / case of 
+        #controls
+        v = ca.SX.sym('v')
+        phi = ca.SX.sym('phi') # the steering rate
+        pass
+    ### desired point is at the center of the rear axle,
+    def kvmodel_rac(self):
 
-### desired point is at the center of the rear axle,
-def kvmodel_rac(L):
-    #states
-    x = ca.SX.sym('x')
-    y = ca.SX.sym('y')
-    psi = ca.SX.sym('psi')      # the yaw angle
-    beta = ca.SX.sym('beta')    # the slip angle angle
-    
-    #controls
-    v = ca.SX.sym('v')
-    delta = ca.SX.sym('delta') # the steering rate
-    
+        
 
-    # State vector and control inputs
-    states = ca.vertcat(x, y, psi, beta)
-    controls = ca.vertcat(v, delta)
+        
 
-    # State update equations (kinematic bicycle model)
-    rhs = ca.vertcat(v * ca.cos(psi)
-                        , v*  ca.sin(psi)
-                        , v/L * ca.tang(delta)
-                        , delta
-                        )
-    
-    return ca.Function('f', [states, controls], [rhs])
+        # State vector and control inputs
+        states = ca.vertcat(x, y, psi, beta)
+        controls = ca.vertcat(v, delta)
 
-### desired point is at the center of the front axle:
-def kvmodel_fac(L):
-    #states
-    x = ca.SX.sym('x')
-    y = ca.SX.sym('y')
-    psi = ca.SX.sym('psi')      # the yaw angle
-    beta = ca.SX.sym('beta')    # the slip angle angle
-    
-    #controls
-    v = ca.SX.sym('v')
-    delta = ca.SX.sym('delta') # the steering rate
-    
+        # State update equations (kinematic bicycle model)
+        rhs = ca.vertcat(v * ca.cos(psi)
+                            , v*  ca.sin(psi)
+                            , v/L * ca.tang(delta)
+                            , delta
+                            )
+        
+        return ca.Function('f', [states, controls], [rhs])
 
-    # State vector and control inputs
-    states = ca.vertcat(x, y, psi, beta)
-    controls = ca.vertcat(v, delta)
+    ### desired point is at the center of the front axle:
+    def kvmodel_fac(L):
+        #states
+        x = ca.SX.sym('x')
+        y = ca.SX.sym('y')
+        psi = ca.SX.sym('psi')      # the yaw angle
+        beta = ca.SX.sym('beta')    # the slip angle angle
+        
+        #controls
+        v = ca.SX.sym('v')
+        delta = ca.SX.sym('delta') # the steering rate
+        
 
-    # State update equations (kinematic bicycle model)
-    rhs = ca.vertcat(v * ca.cos(psi+beta)
-                        , v*  ca.sin(psi+beta)
-                        , v/L * ca.sin(delta)
-                        , delta
-                        )
-    
-    return ca.Function('f', [states, controls], [rhs])
+        # State vector and control inputs
+        states = ca.vertcat(x, y, psi, beta)
+        controls = ca.vertcat(v, delta)
 
-### desired point is at the center of gravity:
-def kvmodel_cg():
+        # State update equations (kinematic bicycle model)
+        rhs = ca.vertcat(v * ca.cos(psi+beta)
+                            , v*  ca.sin(psi+beta)
+                            , v/L * ca.sin(delta)
+                            , delta
+                            )
+        
+        return ca.Function('f', [states, controls], [rhs])
+
+    ### desired point is at the center of gravity:
+    def kvmodel_cg():
     #states
     x = ca.SX.sym('x')
     y = ca.SX.sym('y')

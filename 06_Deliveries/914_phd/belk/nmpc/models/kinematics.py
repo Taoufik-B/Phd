385ff5c9ffc33af86300ce5a132676b4babf9c4b
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-01 17:52:15 +0100

    vault backup: 2024-01-01 17:52:15
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/models/kinematics.py

--- 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
+++ 06_Deliveries/914_phd/belk/nmpc/models/kinematics.py
@@ -10,33 +10,31 @@ class VehicleKinematicModel:
         self.psi = ca.SX.sym('psi')      # the yaw angle / heading angle
         self.delta = ca.SX.sym('delta')  # the steering angle
         
-        self.beta = ca.SX.sym('beta')    # the slip angle angle / case of 
+        self.beta = ca.SX.sym('beta')    # the slip angle angle / case of desired control point is in the cog
         #controls
-        v = ca.SX.sym('v')
-        phi = ca.SX.sym('phi') # the steering rate
+        self.v = ca.SX.sym('v')
+        self.phi = ca.SX.sym('phi') # the steering rate
+
+        # State and Control vector
+        self.states = None
+        self.controls = ca.vertcat(self.v, self.delta)
         pass
     ### desired point is at the center of the rear axle,
     def kvmodel_rac(self):
-
-        
-
-        
-
         # State vector and control inputs
-        states = ca.vertcat(x, y, psi, beta)
-        controls = ca.vertcat(v, delta)
+        states = ca.vertcat(self.x, self.y, self.psi, self.delta)
 
         # State update equations (kinematic bicycle model)
-        rhs = ca.vertcat(v * ca.cos(psi)
-                            , v*  ca.sin(psi)
-                            , v/L * ca.tang(delta)
-                            , delta
-                            )
+        rhs = ca.vertcat( self.v * ca.cos(self.psi)
+                        , self.v*  ca.sin(self.psi)
+                        , self.v/self.L * ca.tan(self.delta)
+                        , self.phi
+                        )
         
-        return ca.Function('f', [states, controls], [rhs])
+        return ca.Function('f', [self.states, self.controls], [rhs])
 
     ### desired point is at the center of the front axle:
-    def kvmodel_fac(L):
+    def kvmodel_fac(self):
         #states
         x = ca.SX.sym('x')
         y = ca.SX.sym('y')

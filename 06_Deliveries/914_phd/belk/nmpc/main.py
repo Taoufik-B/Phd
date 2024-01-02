a11a70d481ed24cf17e191a00a461df189d8592c
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-02 02:03:42 +0100

    vault backup: 2024-01-02 02:03:42
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/main.py
    06_Deliveries/914_phd/belk/nmpc/utils/__pycache__/visualization.cpython-311.pyc
    06_Deliveries/914_phd/belk/nmpc/utils/visualization.py

--- 06_Deliveries/914_phd/belk/nmpc/main.py
+++ 06_Deliveries/914_phd/belk/nmpc/main.py
@@ -8,6 +8,7 @@ from utils.config import load_yaml_config
 from utils.trajectory import ReferenceTrajectory
 from controllers.nmpc_controller import NMPCController
 from models.kinematics import VehicleKinematicModel
+from utils.visualization import simulate
 
 
 def main():
@@ -41,9 +42,27 @@ def main():
         trajectory = ReferenceTrajectory(**config['NMPC.environment']['trajectory'])
         vehicle_model = VehicleKinematicModel(**config['NMPC.externals']['vehicle'])
         nmpc = NMPCController(model=vehicle_model,trajectory=trajectory, **config['NMPC.internals'], bounds=config['NMPC.externals']['bounds'])
+        t=[]
+        reference = trajectory.get_reference()
+        mpciter=0
+        t0 = 0
         ## run the environement
+        while (True):
+            u_opt  = nmpc.compute_controls(mpciter, config['NMPC.externals']['u_ref'])
+            nmpc.run_step(u_opt)
+            distance = np.linalg.norm(trajectory.xs[0:2]-nmpc.x0[0:2])
+            distance_p = np.linalg.norm(trajectory.xs[0:2]-nmpc.p_history[0:2,0,mpciter])
+            logging.info(f"Distance iteration {mpciter} : {distance}")
+            mpciter += 1     
+            t.append(t0)
+            if distance < 0.5:
+                break
+            if distance_p <0.5:
+                break
+            
         ## store the results
         ## plot the results if required
+        simulate(trajectory.path, nmpc.p_history, nmpc.x_history, nmpc.u_opt_history, t, nmpc.dt, nmpc.N,reference, False)
     except KeyboardInterrupt:
         print('\nCancelled by user. Bye!')
 

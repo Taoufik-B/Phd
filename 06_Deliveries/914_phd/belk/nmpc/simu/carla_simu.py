e110d26a1a9e2c83065abdfb38b0d61d20762c54
Taoufik Belkebir <taoufik.belkebir@outlook.de>
2024-01-05 10:02:38 +0100

    vault backup: 2024-01-05 10:02:38
    Affected files:
    06_Deliveries/914_phd/belk/nmpc/simu/__pycache__/__init__.cpython-311.pyc
    06_Deliveries/914_phd/belk/nmpc/simu/__pycache__/carla_simu.cpython-311.pyc
    06_Deliveries/914_phd/belk/nmpc/simu/carla_simu.py

--- 06_Deliveries/914_phd/belk/nmpc/simu/carla_simu.py
+++ 06_Deliveries/914_phd/belk/nmpc/simu/carla_simu.py
@@ -1,10 +1,11 @@
 import sys, glob, os
 
 try:
-    sys.path.append(glob.glob('simu/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+    print("Try Carla import", sys.path)
 except IndexError:
     pass
 
